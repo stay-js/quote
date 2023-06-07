e2808675a974c51c39e40bc8c3d0e67d050894a5
Nagy Zétény <nagyzeteny6@gmail.com>
2023-06-07 21:23:45 +0200

    latest packages, removed @vercel/analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import { Analytics } from '@vercel/analytics/react';
 import { ClientProviders } from './client-providers';
 
 import '~/styles/globals.css';
@@ -6,10 +5,7 @@ import '~/styles/globals.css';
 const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => (
   <html lang="en" className="antialiased">
     <body className="overflow-x-hidden bg-zinc-900 text-white">
-      <ClientProviders>
-        <Analytics />
-        {children}
-      </ClientProviders>
+      <ClientProviders>{children}</ClientProviders>
     </body>
   </html>
 );
